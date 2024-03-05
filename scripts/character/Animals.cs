55dd2f8692a5d5775b8f96f2991e54e537040a26
Michael Museux <michael.museux@epita.fr>
2024-03-06 00:31:10 +0100

    Merged

--- scripts/character/Animals.cs
+++
@@ -1,6 +0,0 @@
-namespace LandsOfAzerith.scripts.character;
-
-public class Animals : Character
-{
-    public override long Health { get; set; }
-}
--- scripts/character/Player.cs
+++ scripts/character/Player.cs
@@ -4,26 +4,18 @@ namespace LandsOfAzerith.scripts.character;
 
 public class Player : Character
 {
-    public int Strength { get; }
-    public int Stamina { get; }
-    public override long Health { get; set; }
-    public int Mana { get; }
+    public override ulong HealthPoints { get; }
+    protected override Character? Aggro { get; }
+    public int Strength { get; private set; }
+    public int Stamina { get; private set; }
+    public int Mana { get; private set; }
     
-    
-
     public Player()
     {
         Strength = 10;
         Stamina = 10;
-        Health = 10;
+        HealthPoints = 10;
         Mana = 10;
     }
-    
-    public override void _Process(double delta)
-    {
-        if (Input.IsKeyPressed(Key.W))
-        {
-            Position += new Vector2(0, -10);
-        }
-    }
+
 }
