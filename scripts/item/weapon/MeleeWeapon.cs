32b76601267f90ebc1c7f930470c294ba880529d
Michael Museux <michael.museux@epita.fr>
2024-03-05 22:26:55 +0100

    Added Range to Weapons

--- scripts/item/weapon/MeleeWeapon.cs
+++ scripts/item/weapon/MeleeWeapon.cs
@@ -3,4 +3,5 @@ namespace LandsOfAzerith.scripts.item.weapon;
 public abstract class MeleeWeapon : Item, IWeapon
 {
     public abstract ulong Damage { get; }
+    public abstract ulong Range { get; }
 }
