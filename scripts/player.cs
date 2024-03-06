5fd653d76d958427be1e8fa367318db2d7e1d0ca
LeDiablePatate <moi.lediable.666@gmail.com>
2024-03-06 18:01:59 +0100

    Fixed player inheritance

--- scripts/player.cs
+++ scripts/player.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class player : Node
+public partial class player : CharacterBody2D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
