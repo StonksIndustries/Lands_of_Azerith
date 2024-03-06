fb5f94f5ad4c7b3fc86dec3dbe5c45d6d972dfb0
LeDiablePatate <moi.lediable.666@gmail.com>
2024-03-06 11:51:21 +0100

    Fixed Godot Dependencies

--- scripts/Character.cs
+++ scripts/Character.cs
@@ -2,9 +2,9 @@ using Godot;
 
 namespace LandsOfAzerith.scripts;
 
-public abstract class Character : CharacterBody2D
+public abstract partial class Character : CharacterBody2D
 {
-    public abstract long Health { get; set; }
-    private Character? _aggro;
-    
+	public abstract long Health { get; set; }
+	private Character? _aggro;
+	
 }
